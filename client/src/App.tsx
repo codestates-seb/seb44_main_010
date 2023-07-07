64a36dbe18fff8d3aba470d6f37d72fa1188c07b
coaudtn0276 <coaudtn0276@gmail.com>
2023-07-07 21:52:05 +0900

    fix: 전역 폰트 설정 전역 폰트 설정, media쿼리를 이용한 반응형 폰트크기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "../src/components/default/Nav.tsx";
-import Footer from "../src/components/default/Footer.tsx"
+import Footer from "../src/components/default/Footer.tsx";
 import IntroductionPage from "../src/pages/introduction/introductionPage.tsx";
 import LoginPage from "../src/pages/login/loginPage.tsx";
 import SignupPage from "../src/pages/signup/signupPage.tsx";
@@ -13,7 +13,6 @@ import CalendarPage from "../src/pages/consumption/calendarPage.tsx";
 import SummaryPage from "../src/pages/consumption/summaryPage.tsx";
 import ErrorPage from "../src/pages/error/404Page.tsx";
 
-
 function App() {
   return (
     <>
@@ -22,28 +21,33 @@ function App() {
         <Routes>
           {/*서비스 소개페이지 */}
           <Route path="/" element={<IntroductionPage />}></Route>
-           
-           {/*로그인 페이지 */}
-           <Route path="/users/login" element={<LoginPage />}></Route>
-           {/*회원가입 페이지 */}
-           <Route path="/users/sign_up" element={<SignupPage />}></Route>
-           {/*가입완료 페이지*/}
-           <Route path="/completed" element={<Completed />}></Route>
 
-           {/*자산 페이지*/}
-           <Route path="/asset" element={<AssetPage />}></Route>
+          {/*로그인 페이지 */}
+          <Route path="/users/login" element={<LoginPage />}></Route>
+          {/*회원가입 페이지 */}
+          <Route path="/users/sign_up" element={<SignupPage />}></Route>
+          {/*가입완료 페이지*/}
+          <Route path="/completed" element={<Completed />}></Route>
 
-           {/*소비 일일페이지 */}
-           <Route path="/consumption/day_upload" element ={<DayPage />}></Route>
-           {/*소비 월별페이지 */}
-           <Route path="/consumption/month_upload" element ={<MonthPage />}></Route>
-           {/*소비 달력페이지 */}
-           <Route path="/consumption/calendar" element ={<CalendarPage />}></Route>
-           {/*소비 요약페이지 */}
-           <Route path="/consumption/summary" element ={<SummaryPage />}></Route>
-           {/* 404 페이지 */}
-           <Route path="*" element ={<ErrorPage />}></Route>
-        
+          {/*자산 페이지*/}
+          <Route path="/asset" element={<AssetPage />}></Route>
+
+          {/*소비 일일페이지 */}
+          <Route path="/consumption/day_upload" element={<DayPage />}></Route>
+          {/*소비 월별페이지 */}
+          <Route
+            path="/consumption/month_upload"
+            element={<MonthPage />}
+          ></Route>
+          {/*소비 달력페이지 */}
+          <Route
+            path="/consumption/calendar"
+            element={<CalendarPage />}
+          ></Route>
+          {/*소비 요약페이지 */}
+          <Route path="/consumption/summary" element={<SummaryPage />}></Route>
+          {/* 404 페이지 */}
+          <Route path="*" element={<ErrorPage />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ client/src/pages/consumption/calendarPage.tsx
@@ -0,0 +1,3 @@
+export default function CalenderPage() {
+  return <div></div>;
+}
