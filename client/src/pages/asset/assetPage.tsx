496856b8e52807b50c428ae24fc909f0a4017a55
taehyeon83 <wxg5483@naver.com>
2023-07-10 21:21:01 +0900

    Chore: 자산 페이지 내 컴포넌트 배치

--- client/src/pages/asset/assetPage.tsx
+++ client/src/pages/asset/assetPage.tsx
@@ -1,3 +1,24 @@
+import * as S from "../../pages/asset/assetPageStyled";
+import AssetHeader from "../../components/card/AssetHeader";
+import SavingAccount from "../../components/card/SavingAccount";
+import Stock from "../../components/card/Stock";
+import AddProperty from "../../components/card/AddProperty";
+import AddCar from "../../components/card/AddCar";
+import AddCash from "../../components/card/AddCash";
+
 export default function AssetPage() {
-  return <div></div>;
+  return (
+    <S.AssetPageContainer>
+      <AssetHeader />
+      <S.SavingAccountBox>
+        <SavingAccount />
+        <Stock />
+        <S.AddButtons>
+          <AddProperty />
+          <AddCar />
+          <AddCash />
+        </S.AddButtons>
+      </S.SavingAccountBox>
+    </S.AssetPageContainer>
+  );
 }
