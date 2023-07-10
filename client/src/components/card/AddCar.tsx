6ac44e2cb71ce81c53fb52d028f51a68b7d5f4a4
taehyeon83 <wxg5483@naver.com>
2023-07-10 21:16:19 +0900

    Chore: 추가하기 버튼 생성

---
+++ client/src/components/card/AddCar.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import Car from "../../assets/Car.svg";
+
+const Main = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  height: 20vh;
+  width: 8vw;
+  border-radius: 3rem;
+  box-shadow: 0px 4px 13px 0px rgb(0, 0, 0, 0.1);
+  border: 1px solid #d9d9d9;
+  padding: 3rem;
+  margin: 5rem;
+  &:hover {
+    background-color: #ffce0b;
+    color: #ffffff;
+  }
+`;
+
+const CarImg = styled.img`
+  cursor: pointer;
+  width: auto;
+  height: auto;
+`;
+const Title = styled.div`
+  font-size: 3rem;
+`;
+export default function AddCar() {
+  return (
+    <Main>
+      <CarImg src={Car}></CarImg>
+      <Title>자동차 추가하기</Title>
+    </Main>
+  );
+}
---
+++ client/src/components/card/AddProperty.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import Property from "../../assets/Property.svg";
+const Main = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  height: 20vh;
+  width: 8vw;
+  border-radius: 3rem;
+  box-shadow: 0px 4px 13px 0px rgb(0, 0, 0, 0.1);
+  border: 1px solid #d9d9d9;
+  padding: 3rem;
+  margin: 5rem;
+  &:hover {
+    background-color: #ffce0b;
+    color: #ffffff;
+  }
+`;
+
+const PropertyImg = styled.img`
+  cursor: pointer;
+  width: auto;
+  height: auto;
+`;
+const Title = styled.div`
+  font-size: 3rem;
+`;
+export default function AddProperty() {
+  return (
+    <Main>
+      <PropertyImg src={Property}></PropertyImg>
+      <Title>부동산 추가하기</Title>
+    </Main>
+  );
+}
