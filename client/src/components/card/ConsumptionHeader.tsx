36cef604d9a2be150ebbfdf77773c1dc0a0e78f2
정지은 <kindjjee@gmail.com>
2023-07-11 10:31:35 +0900

    Chore: 컴포넌트 배치

--- client/src/components/card/ConsumptionHeader.tsx
+++ client/src/components/card/ConsumptionHeader.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import 소비배경 from "../../assets/소비배경.svg";
+import 소비배경 from "../../assets/소비배경.svg";
 
 const Main = styled.div`
   display: flex;
@@ -21,7 +21,7 @@ const WordsContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin-left: 20vw;
+  margin-left: 10vw;
 
   .소비 {
     font-size: 7rem;
@@ -48,3 +48,4 @@ export default function ConsumptionHeader() {
     </Main>
   );
 }
+
