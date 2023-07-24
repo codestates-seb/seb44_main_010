f602f9e0d524f1830c9d9532612c03c6f2054c47
정지은 <kindjjee@gmail.com>
2023-07-24 16:33:03 +0900

    FIX: commit해야 컨플릭 안나서

--- client/src/pages/asset/assetPage.tsx
+++ client/src/pages/asset/assetPage.tsx
@@ -45,7 +45,9 @@ export default function AssetPage() {
       })
       .catch((err) => {
         if (err.response) {
-          const errMessage = (err.response as AxiosResponse<{ message: string }>)?.data.message;
+          const errMessage = (
+            err.response as AxiosResponse<{ message: string }>
+          )?.data.message;
           window.alert(errMessage);
           console.log(errMessage);
         } else {
@@ -69,7 +71,7 @@ export default function AssetPage() {
             {assetdata && <S.Title>부동산</S.Title>}
             {assetdata && <Property assetdata={assetdata} />}
             {assetdata && <S.Title>자동차</S.Title>}
-            {assetdata &&<Car assetdata={assetdata} />}
+            {assetdata && <Car assetdata={assetdata} />}
             {assetdata && <S.Title>현금</S.Title>}
             {assetdata && <Cash assetdata={assetdata} />}
             <S.AddButtons>
